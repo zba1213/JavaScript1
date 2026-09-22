7f021b250ebb8f424dab5488279ce1586ffbd4a1
zba1213 <amini_zabi@hotmail.com>
2026-09-22 10:49:31 +0200

    opdateret hjemmeside

--- js/index.js
+++ js/index.js
@@ -1,2 +1,20 @@
 //Write your Javascript code here
 console.log("Shoppinglist")
+
+function CreateLi(className, id, Text) 
+{
+   let newLi = document.createElement("li"); 
+
+   newLi.setAttribute("id", id);
+
+   newLi.setAttribute("class", className);
+
+   let textNode = document.createTextNode(Text);
+
+   newLi.appendChild(textNode);
+
+   return newLi;
+}
+
+let myLi = CreateLi("item", 1, "Good Pizza");
+document.getElementById("list").appendChild(myLi);
